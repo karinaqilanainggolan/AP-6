9a55ba103a064edb94c4c695a60326fa131036d8
karinaqilanainggolan <karinaqila15062005@gmail.com>
2024-03-07 19:25:24 -0800

    A

---
+++ MyFirstProgram.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+    string NAMa[50];
+    int NIM;
+    char KOM;
+    float IP;
+
+
+    printf("Hello World|\n");
+
+    printf("Masukkan NAMA : ");
+    scanf("%s",&nama);
+
+    gets(nama);
+
+    printf("Masukkan NIM : ");
+    scanf("%d",&nim);
+
+    printf("Masukkan KOM : ");
+    scanf("%c",&kom);
+
+    printf("Masukkan IP : ");
+    scanf("%f",&ip)
+
+    printf("NAMA : ");
+    puts(nama); //
+
+
+    printf("NAMA : %s\n, nama");
+    printf("NIM : %d\n", nim);
+    printf("KOM: %c\n", kom);
+    printf("IP: %f\n", ip);
+    printf("Hello world");
+
+
+
+
+
+
+
+}
